3e5d2a073fb2f63afabd0249d4146f56aac1514a
Serge Pille <27204764+SergeP70@users.noreply.github.com>
2017-12-20 15:30:22 +0100

    LocationList in Picker

--- B4.PE3.PilleS/B4.PE3.PilleS/ViewModels/TrackingViewModel.cs
+++ B4.PE3.PilleS/B4.PE3.PilleS/ViewModels/TrackingViewModel.cs
@@ -14,27 +14,60 @@ namespace B4.PE3.PilleS.ViewModels
 {
     public class TrackingViewModel : INotifyPropertyChanged
     {
-        // Deze ViewModel zal de TrackingContentView (MainView) ondersteunen en bevat bijgevolg een collectie van Location objecten:
+        // Deze ViewModel zal de TrackingContentView ondersteunen en bevat bijgevolg een collectie van LocationList en Location objecten:
         private INavigation navigation;
         private LocationInMemoryService locationService;
         private LocationList locationList, selectedLocationList;
         private Location location;
         public ICommand ShowLocationDetailsCommand { get; private set; }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public TrackingViewModel(INavigation navigation)
         {
             this.navigation = navigation;
             locationService = new LocationInMemoryService();
 
-            // Initialize the location collection, take the first LocationList
-            selectedLocationList = (LocationList)(locationService.GetLocationLists().Result.FirstOrDefault());
-            Locations = new ObservableCollection<Location>(locationService.GetByListName(selectedLocationList.ListName).Result);
+            // Initialize the location collection, 
+            LocationLists = new ObservableCollection<LocationList>(locationService.GetAll().Result);
+            
+            // take the first LocationList
+            selectedLocationList = (LocationList)(locationService.GetLocationLists().Result.Where(l => l.ListName == "Ronde 1").FirstOrDefault());
+            // vervang door: selectedLocationList = (LocationList)(locationService.GetLocationLists().Result.FirstOrDefault());
+            //this.ListName = selectedLocationList.ListName;
+
+            if (locationService.GetByListName(selectedLocationList.ListName).Result == null)
+                Locations = null;
+            else
+                Locations = new ObservableCollection<Location>(locationService.GetByListName(selectedLocationList.ListName).Result);
 
             // Listen to the messaging center to inform if a location is saved and refresh the Listview
             MessagingCenter.Subscribe(this, MessageLocations.LocationSaved,
-                async (LocationViewModel sender, Location loc) => {
+                async (LocationViewModel sender, Location loc) =>
+                {
                     Locations = new ObservableCollection<Location>(await locationService.GetByListName(selectedLocationList.ListName));
                 });
+        }
+
+        //private string listName;
+        //public string ListName
+        //{
+        //    get { return listName; }
+        //    set
+        //    {
+        //        listName = value;
+        //        RaisePropertyChanged(nameof(ListName));
+        //    }
+        //}
+
+        private ObservableCollection<LocationList> locationLists;
+        public ObservableCollection<LocationList> LocationLists
+        {
+            get { return locationLists; }
+            set
+            {
+                locationLists = value;
+                RaisePropertyChanged(nameof(LocationLists));
+            }
         }
 
         private ObservableCollection<Location> locations;
@@ -48,7 +81,23 @@ namespace B4.PE3.PilleS.ViewModels
             }
         }
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        private int locationListSelectedIndex;
+        public int LocationListSelectedIndex
+        {
+            get { return locationListSelectedIndex; }
+            set
+            {
+                if (locationListSelectedIndex != value)
+                {
+                    locationListSelectedIndex = value;
+                    // trigger some action to take
+                    RaisePropertyChanged(nameof(LocationListSelectedIndex));
+                    selectedLocationList = LocationLists[locationListSelectedIndex];
+                    Locations = new ObservableCollection<Location>(locationService.GetByListName(selectedLocationList.ListName).Result);
+                }
+            }
+        }
+
 
         /// <summary>
         /// Raises the PropertyChanged event if it has handlers attached to it
@@ -61,7 +110,8 @@ namespace B4.PE3.PilleS.ViewModels
         }
 
         public ICommand SortCommand => new Command(
-            async () => {
+            async () =>
+            {
                 // refresh the list and sort data by Time
                 var sortedLocations = (await locationService.GetByListName(selectedLocationList.ListName)).OrderBy(e => e.LocationTime).ToList();
                 // reset the collection
@@ -69,17 +119,18 @@ namespace B4.PE3.PilleS.ViewModels
             });
 
         public ICommand ViewLocationDetailCommand => new Command<Location>(
-            (Location loc) => {
+            (Location loc) =>
+            {
 
                 navigation.PushAsync(new LocationDetailView(selectedLocationList, loc));
-                
+
             });
 
         public ICommand RefreshCommand => new Command(
             async () =>
             {
-            Locations = new ObservableCollection<Location>(
-                (await locationService.GetByListName(selectedLocationList.ListName)).OrderBy(e => e.LocationTime).ToList());
+                Locations = new ObservableCollection<Location>(
+                    (await locationService.GetByListName(selectedLocationList.ListName)).OrderBy(e => e.LocationTime).ToList());
             });
 
         public ICommand SaveLocationCommand => new Command(
